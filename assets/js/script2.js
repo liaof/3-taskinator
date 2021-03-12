8ea4dc60f5ac02f5759a2b82dde2c4a2797a8ea1
liaof <f.liao163@gmail.com>
2021-03-11 22:33:16 -0500

    2 versions of createTaskHandler

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,5 +1,3 @@
-/*the .document object allows us to access everything on the webpage including metadata*/
-/*it represents the index.html of the current page*/
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
@@ -16,16 +14,14 @@ var createTaskHandler = function() {
     //create list item
     var listItemEl = document.createElement("li"); 
     listItemEl.className = "task-item";
-    //create div to hold name and type info and add to list item above
-    var taskInfoEl = document.createElement("div");
-    taskInfoEl.className = "task-info";
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3> <span class='task-type'>" + taskTypeInput + "</span>";
+    
+    //add name and type directly to listItemEl
+    listItemEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3> <span class='task-type'>" + taskTypeInput + "</span>";
 
     //put the container containing name and type info into our
-    listItemEl.appendChild(taskInfoEl);
+
     tasksToDoEl.appendChild(listItemEl);
 
 }   
 
 formEl.addEventListener("submit", createTaskHandler);
-
